f3ccef5ff9cb0792ae94280d6aa7d242de273281
Bishal Pandey <bishalpandey.im@gmail.com>
2026-07-14 14:54:57 +0545

    simple gallery project

---
+++ 17-gallery-project/src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  //
+  //
+  const [userData, setUserData] = useState([]);
+
+  const getData = async () => {
+    const response = await axios.get(
+      "https://picsum.photos/v2/list?page=3&limit=30",
+    );
+
+    setUserData(response.data);
+  };
+
+  let printUserData = "No User Available";
+
+  if (userData.length > 0) {
+    printUserData = userData.map(function (elem, idx) {
+      return (
+        <div key={idx}>
+          <div className="h-40 w-44 overflow-hidden bg-white rounded-xl">
+            <img
+              className="h-full w-full object-cover "
+              src={elem.download_url}
+              alt=""
+            />
+          </div>
+          <h2 className="font-bold text-lg">{elem.author}</h2>
+        </div>
+      );
+    });
+  }
+
+  //
+  //
+  return (
+    <div className="bg-black overflow-auto h-screen p-4 text-white">
+      <button
+        onClick={getData}
+        className="bg-green-600 active:scale-95 mb-3 px-5 py-2 rounded text-white"
+      >
+        Get Data
+      </button>
+
+      {/* mathi var ma image halexam hamle  */}
+      <div className="flex flex-wrap gap-4">{printUserData}</div>
+    </div>
+  );
+};
+
+export default App;
