fdfaccd0043a0e812c6e32a4baccf8d012e8a577
Bishal Pandey <bishalpandey.im@gmail.com>
2026-07-16 17:11:26 +0545

    Gallery project

--- 17-gallery-project/src/App.jsx
+++ 17-gallery-project/src/App.jsx
@@ -1,33 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Card from "./components/card";
 
 const App = () => {
   //
   //
   const [userData, setUserData] = useState([]);
 
+  const [index, setIndex] = useState(1);
+
   const getData = async () => {
     const response = await axios.get(
-      "https://picsum.photos/v2/list?page=3&limit=30",
+      `https://picsum.photos/v2/list?page=${index}&limit=10`,
     );
 
     setUserData(response.data);
   };
 
-  let printUserData = "No User Available";
+  useEffect(
+    function () {
+      getData();
+    },
+    [index],
+  ); // tala button ma tw setIndex garem but we need to reload so use this
+
+  let printUserData = <h3>Loading ............</h3>;
 
   if (userData.length > 0) {
     printUserData = userData.map(function (elem, idx) {
       return (
         <div key={idx}>
-          <div className="h-40 w-44 overflow-hidden bg-white rounded-xl">
-            <img
-              className="h-full w-full object-cover "
-              src={elem.download_url}
-              alt=""
-            />
-          </div>
-          <h2 className="font-bold text-lg">{elem.author}</h2>
+          <Card elem={elem} />
         </div>
       );
     });
@@ -36,16 +39,35 @@ const App = () => {
   //
   //
   return (
-    <div className="bg-black overflow-auto h-screen p-4 text-white">
-      <button
-        onClick={getData}
-        className="bg-green-600 active:scale-95 mb-3 px-5 py-2 rounded text-white"
-      >
-        Get Data
-      </button>
-
+    <div clas  sName="bg-black overflow-auto h-screen p-4 text-white ">
       {/* mathi var ma image halexam hamle  */}
-      <div className="flex flex-wrap gap-4">{printUserData}</div>
+      <div className="flex flex-wrap gap-4 p-2">{printUserData}</div>
+
+      {/* yo chai Button ko lagi */}
+
+      <div className="flex justify-center gap-6 items-center p-4">
+        <button
+          className="bg-amber-400  text-sm cursor-pointer active:scale-95  text-black rounded px-4 py-2 font-semibold"
+          onClick={() => {
+            if (index > 1) {
+              setIndex(index - 1);
+              setUserData([]);
+            }
+          }}
+        >
+          Prev
+        </button>
+        <h4>Page {index}</h4>
+        <button
+          className="bg-amber-400 text-sm cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold "
+          onClick={() => {
+            setIndex(index + 1);
+            setUserData([]);
+          }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
